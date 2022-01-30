f521b02f3c104117620e531c4218b6fa85bb8bb6
emuhlestein <emuhlestein@yahoo.com>
2022-01-30 05:54:20 -0700

    Add PathVariable and RequestParameter.

--- simple-spring-boot/src/main/java/com/intelliviz/simplespringboot/SimpleSpringBootApplication.java
+++ simple-spring-boot/src/main/java/com/intelliviz/simplespringboot/SimpleSpringBootApplication.java
@@ -2,11 +2,11 @@ package com.intelliviz.simplespringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/hello")
 public class SimpleSpringBootApplication {
 
 	public static void main(String[] args) {
@@ -16,6 +16,16 @@ public class SimpleSpringBootApplication {
 	@RequestMapping
 	public String hello() {
 		return "Hello from Spring Boot";
+	}
+
+	@GetMapping("/{name}")
+	public String helloName(@PathVariable String name) {
+		return "Hello, " + name + ", from Spring Boot using PathVariable";
+	}
+
+	@GetMapping("")
+	public String helloName2(@RequestParam String name) {
+		return "Hello, " + name + ", from Spring Boot using RequestParam";
 	}
 
 }
